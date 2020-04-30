312fbefd7af6d77961816fc624e045ce6b97778c
Ankur Prakash <mac.ankurprakash@gmail.com>
2020-04-30 07:11:31 +0530

    Created template DS project

---
+++ DataStructures/DataStructures/DataStructures.h
@@ -0,0 +1,19 @@
+//
+//  DataStructures.h
+//  DataStructures
+//
+//  Created by Ankur Prakash on 30/04/20.
+//  Copyright © 2020 Ankur Prakash. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DataStructures.
+FOUNDATION_EXPORT double DataStructuresVersionNumber;
+
+//! Project version string for DataStructures.
+FOUNDATION_EXPORT const unsigned char DataStructuresVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DataStructures/PublicHeader.h>
+
+
